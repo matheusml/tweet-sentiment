23c0ff5d17ae64a2cb43eb39c7072c0c0d4a1909
Matheus Lima <matheusml90@gmail.com>
2013-10-15 13:28:26 -0300

    Adds twitter consumer keys to project

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,12 @@ class HomeController < ApplicationController
 
 	def analyze
 		puts "-- #{params}"
+		client = Twitter::REST::Client.new do |config|
+		  config.consumer_key        = "BlpfM8bCI4RVELlc5PGhAg"
+		  config.consumer_secret     = "IJYJ0ga6CP4sNBZ7pCgCFh73aocPXCTmbIKLYVbomIQ"
+		  config.access_token        = "15689757-hspmJBwuytAkFlJzKNUpvCIV0skcQbDyCKvrgTLag"
+		  config.access_token_secret = "0lufFh9k1j5mQ2DtJ2PswvGIJrZTQfsbxkau0Gp6U0"
+		end
 	end
 	
 end
