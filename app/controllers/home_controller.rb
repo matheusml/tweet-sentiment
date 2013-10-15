07375c121f47d906bad1191e8b91902c0a9dad9c
Matheus Lima <matheusml90@gmail.com>
2013-10-15 11:47:11 -0300

    Fixes layout of index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,9 @@ class HomeController < ApplicationController
 
 	def index
 	end
+
+	def analyze
+		puts "-- #{params}"
+	end
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 TweetSentiment::Application.routes.draw do
   root :to => 'home#index'  
+
+  match "/analyze" => 'home#analyze', :as => :analyze, :via => :post 
 end
