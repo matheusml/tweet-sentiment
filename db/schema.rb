cd6072d15bac5ce12ad8532d69a10b486a8ee20f
Matheus Lima <matheusml90@gmail.com>
2013-10-26 16:42:32 -0200

    Populates Contexts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131026183743) do
+ActiveRecord::Schema.define(version: 20131026184054) do
 
   create_table "contexts", force: true do |t|
     t.string   "name"
