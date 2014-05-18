5cb47c7a8dd34a11cb6345ce9cbf0c7c0a9a4f51
Matheus Lima <matheusml90@gmail.com>
2014-05-18 12:03:16 -0300

    Removes get tweets route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ TweetSentiment::Application.routes.draw do
 
   match "/analyze" => 'home#analyze', :as => :analyze, :via => :post 
   match "/result/:search" => 'home#result', :as => :result, :via => :get 
-  match "/get_tweets" => 'home#get_tweets', :as => :get_tweets, :via => :get 
 end
